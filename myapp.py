e17f0ff4640ca87c027cd96de11b8b0161cce886
中村誠司 <masashi@MasashiMacBook-Pro.local>
2017-05-02 20:50:25 +0900

    first commit

---
+++ myapp.py
@@ -0,0 +1,17 @@
+
+# -*- coding: utf-8 -*-
+# import sys
+# import codec
+#comment
+msg = "Hello World"
+print(msg);
+msg = "Hello Again"
+print(msg);
+
+#
+# s="he\nllo wor\tld"
+# html = """<html>
+# <body></body>
+# </html>"""
+# print(s)
+# print(html)
